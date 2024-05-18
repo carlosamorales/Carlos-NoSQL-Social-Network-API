51bb841811cbc963d7017acd865e78e595f45078
Carlos Asencio Morales <carlosgkt@gmail.com>
2024-05-18 18:06:22 -0500

    Updating the function findandremove with findByIdAndDelete

--- controllers/userController.js
+++ controllers/userController.js
@@ -42,7 +42,7 @@ module.exports = {
   },
   // Delete a user by ID
   deleteUser(req, res) {
-    User.findByIdAndRemove(req.params.userId)
+    User.findByIdAndDelete(req.params.userId)
       .then((user) =>
         !user
           ? res.status(404).json({ message: 'No user with this id!' })
